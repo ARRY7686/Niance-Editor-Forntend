42907f7cf6d673a586d3f0526b43c77f4bd8cc50
Akshat Sipany <borncancer21@gmail.com>
2025-02-23 17:02:41 +0530

    implementing about page and fixing editor

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Hero from "../components/Hero";
 import Editor from "../pages/Editor";
+import About from "../pages/About";
 
 const AppRoutes: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<Hero />} />
-      <Route path="/about" element={<h1>About</h1>} />
+      <Route path="/about" element={<About />} />
       <Route path="/project" element={<Editor/>} />
       <Route path="/contribute" element={<h1>Contribute</h1>} />
     </Routes>
